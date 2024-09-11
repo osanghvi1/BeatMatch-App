243524acd8622176b630f283a694362c5db9f887
Lawson <lport@iastate.edu>
2024-09-10 23:22:50 -0500

    small change to unit_1_helloworld

--- Experiments/lawson/unit_1/tutorials-springboot_unit1_1_helloworld/helloworld/src/main/java/coms309/WelcomeController.java
+++ Experiments/lawson/unit_1/tutorials-springboot_unit1_1_helloworld/helloworld/src/main/java/coms309/WelcomeController.java
@@ -7,12 +7,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RestController
 class WelcomeController {
 
+
+    //generated when your search in localhost has nothing after the numbers or
+    //the optional '/'
     @GetMapping("/")
     public String welcome() {
         return "Hello and welcome to COMS 309";
     }
 
+    //generated when something is specified after the numbers and the '/', and will
+    //display the text after the dash as a name for the greeting
     @GetMapping("/{name}")
+
+    //chage: if the string contains any numbers, 
     public String welcome(@PathVariable String name) {
         return "Hello and welcome to COMS 309: " + name;
     }
