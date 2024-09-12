be23f970ca944cde9f7e5256ddb6b8b9975ad758
Griffin <griffinu@iastate.edu>
2024-09-12 15:27:56 -0500

    more exp3 changes, deleted a test file

--- Experiments/griffinu/unit1/tutorials-android_unit1_3_login_signup/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
+++ Experiments/griffinu/unit1/tutorials-android_unit1_3_login_signup/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.androidexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -17,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
     private Button loginButton;     // define login button variable
     private Button signupButton;    // define signup button variable
 
+    //EXPERIMENT CHANGE
+    private Switch switch1;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         usernameText = findViewById(R.id.main_username_txt);// link to username textview in the Main activity XML
         loginButton = findViewById(R.id.main_login_btn);    // link to login button in the Main activity XML
         signupButton = findViewById(R.id.main_signup_btn);  // link to signup button in the Main activity XML
+        switch1 = findViewById(R.id.switch1);
 
         /* extract data passed into this activity from another activity */
         Bundle extras = getIntent().getExtras();
@@ -61,5 +68,21 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }
+
+        //EXPERIMENT switch listener, when flipped will change the color of the screen to darker
+        switch1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (switch1.isChecked()) {
+
+                } else {
+
+                }
+            }
+        });
+
+        }
+
+
 }
