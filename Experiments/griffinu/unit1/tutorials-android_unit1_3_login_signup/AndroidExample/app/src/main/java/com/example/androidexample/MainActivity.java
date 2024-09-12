978259476dbdf94090b6e35b1639a4000cc8064d
Griffin <griffinu@iastate.edu>
2024-09-12 16:14:45 -0500

    messing around with experiements more

--- Experiments/griffinu/unit1/tutorials-android_unit1_3_login_signup/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
+++ Experiments/griffinu/unit1/tutorials-android_unit1_3_login_signup/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Switch;
 import android.widget.TextView;
 
@@ -19,8 +20,7 @@ public class MainActivity extends AppCompatActivity {
     private Button loginButton;     // define login button variable
     private Button signupButton;    // define signup button variable
 
-    //EXPERIMENT CHANGE
-    private Switch switch1;
+
 
 
     @Override
@@ -33,7 +33,9 @@ public class MainActivity extends AppCompatActivity {
         usernameText = findViewById(R.id.main_username_txt);// link to username textview in the Main activity XML
         loginButton = findViewById(R.id.main_login_btn);    // link to login button in the Main activity XML
         signupButton = findViewById(R.id.main_signup_btn);  // link to signup button in the Main activity XML
-        switch1 = findViewById(R.id.switch1);
+        //EXPERIMENT
+
+
 
         /* extract data passed into this activity from another activity */
         Bundle extras = getIntent().getExtras();
@@ -69,18 +71,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //EXPERIMENT switch listener, when flipped will change the color of the screen to darker
-        switch1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if (switch1.isChecked()) {
-
-                } else {
-
-                }
-            }
-        });
 
         }
 
