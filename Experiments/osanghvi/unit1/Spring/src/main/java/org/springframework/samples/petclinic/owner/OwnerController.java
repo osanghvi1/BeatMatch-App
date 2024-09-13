f8895237975f5d1986f64c82c98aaca29174c23c
osanghvi <ombakugan@gmail.com>
2024-09-13 09:53:50 -0500

    Final Spring

--- Experiments/osanghvi/unit1/Spring/src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
+++ Experiments/osanghvi/unit1/Spring/src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
@@ -9,7 +9,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIeS OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
@@ -18,10 +18,7 @@ package org.springframework.samples.petclinic.owner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -43,7 +40,7 @@ class OwnerController {
     private final Logger logger = LoggerFactory.getLogger(OwnerController.class);
 
     @RequestMapping(method = RequestMethod.POST, path = "/owners/new")
-    public String saveOwner(Owners owner) {
+    public String saveOwner(@RequestBody Owners owner) {
         ownersRepository.save(owner);
         return "New Owner "+ owner.getFirstName() + " Saved";
     }
@@ -54,10 +51,12 @@ class OwnerController {
         Owners o2 = new Owners(2, "Jane", "Doe", "Its a secret", "you wish");
         Owners o3 = new Owners(3, "Some", "Pleb", "Right next to the Library", "515-345-41213");
         Owners o4 = new Owners(4, "Chad", "Champion", "Reddit memes corner", "420-420-4200");
+        Owners o5 = new Owners(5, "Om", "Sanghvi", "Reddits memes corner", "426-420-4206");
         ownersRepository.save(o1);
         ownersRepository.save(o2);
         ownersRepository.save(o3);
         ownersRepository.save(o4);
+        ownersRepository.save(o5);
         return "Successfully created dummy data";
     }
 
