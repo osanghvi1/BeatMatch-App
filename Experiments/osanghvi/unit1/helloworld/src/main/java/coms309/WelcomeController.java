9003f93dc4879fc888a7ca686135ded1786761d6
osanghvi1 <ombakugan@gmail.com>
2024-09-13 10:26:04 -0500

    Fixed helloworld master

--- Experiments/osanghvi/unit1/helloworld/src/main/java/coms309/WelcomeController.java
+++ Experiments/osanghvi/unit1/helloworld/src/main/java/coms309/WelcomeController.java
@@ -9,7 +9,8 @@ class WelcomeController {
 
     @GetMapping("/")
     public String welcome() {
-        return "Hello and welcome to COMS 309";
+        return "<strong>Hello</strong> and welcome to <span style = 'color: Blue; font-size:25px; font: '>BeatMatch</span>.\n" + "We are so happy to see you!";
+
     }
 
     @GetMapping("/{name}")
