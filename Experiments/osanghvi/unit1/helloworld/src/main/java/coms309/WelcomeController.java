e279e99210bbfdfd980d456902b77eef6517f414
osanghvi <ombakugan@gmail.com>
2024-09-13 12:36:27 -0500

    helloWorld final

--- Experiments/osanghvi/unit1/helloworld/src/main/java/coms309/WelcomeController.java
+++ Experiments/osanghvi/unit1/helloworld/src/main/java/coms309/WelcomeController.java
@@ -9,7 +9,7 @@ class WelcomeController {
 
     @GetMapping("/")
     public String welcome() {
-        return "<strong>Hello</strong> and welcome to <span style = 'color: Blue; font-size:25px; font: '>BeatMatch</span>.\n" + "We are so happy to see you!";
+        return "<strong>Hello</strong> and welcome to <span style = 'color: Blue; font-size:25px; font: '>BeatMatch</span>.\n" + "<br></br>We are so happy to see you!";
 
     }
 
