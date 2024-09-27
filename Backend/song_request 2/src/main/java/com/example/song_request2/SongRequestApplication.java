0b249f9b1276bfbd9c983a5826b3b304aa46cb87
osanghvi <ombakugan@gmail.com>
2024-09-27 15:02:28 -0500

    adding files + database connection

---
+++ Backend/song_request 2/src/main/java/com/example/song_request2/SongRequestApplication.java
@@ -0,0 +1,13 @@
+package com.example.song_request2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongRequestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SongRequestApplication.class, args);
+    }
+
+}
---
+++ Backend/song_request 2/src/test/java/com/example/song_request2/SongRequestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.song_request2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongRequestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
