17e6c51f2892a81ed7a2b24167ff75a9addc64c6
Griffin <148525371+griffinurban03@users.noreply.github.com>
2024-09-30 16:24:05 -0500

    added variables for buttons

--- Frontend/OpeningActivity/app/src/main/java/com/example/openingactivity/MainActivity.java
+++ Frontend/OpeningActivity/app/src/main/java/com/example/openingactivity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.openingactivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,18 +11,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button Login_Button, Signup_Button;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-
+        Login_Button = findViewById(R.id.log_in_button);
+        Signup_Button = findViewById(R.id.sign_up_button);
 
 
 
