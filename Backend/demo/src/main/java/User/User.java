3c77af040e2b832d08fab142caa24954aa816cbb
Lawson <lport@iastate.edu>
2024-09-30 15:45:02 -0500

    adding stuff for login and signup

---
+++ Backend/demo/src/main/java/User/User.java
@@ -0,0 +1,86 @@
+package User;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userID;
+    private String userName;
+    private int password;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private int securityChoice;
+    private String securityAnswer;
+    private int accountVisibility;
+    private int accountStatus;
+
+    public User(String firstName, String lastName, String email, String username, int password, int securityChoice, String securityAnswer, int accountVisibility, int accountStatus) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.userName = username;
+        this.password = password;
+        this.securityChoice = securityChoice;
+        this.securityAnswer = securityAnswer;
+        this.accountVisibility = accountVisibility;
+        this.accountStatus = accountStatus;
+    }
+
+    public User() {
+    }
+
+
+    // ================ Getters and setters ==========================//
+    public int getUserID() {return userID;}
+
+    public int getPassword() {return password;}
+
+    public String getUserName() {return userName;}
+
+    public String getEmail() {return email;}
+
+    public String getFirstName() {return firstName;}
+
+    public String getLastName() {return lastName;}
+
+    public int getSecurityChoice() {return securityChoice;}
+
+    public String getSecurityAnswer() {return securityAnswer;}
+
+    public int getAccountVisibility() {return accountVisibility;}
+
+    public int getAccountStatus() {return accountStatus;}
+
+    public void setUserID(int userID) {this.userID = userID;}
+
+    public void setPassword(int password) {this.password = password;}
+
+    public void setUserName(String userName) {this.userName = userName;}
+
+    public void setEmail(String email) {this.email = email;}
+
+    public void setFirstName(String firstName) {this.firstName = firstName;}
+
+    public void setLastName(String lastName) {this.lastName = lastName;}
+
+    public void setSecurityChoice(int securityChoice) {this.securityChoice = securityChoice;}
+
+    public void setSecurityAnswer(String securityAnswer) {this.securityAnswer = securityAnswer;}
+
+    public void setAccountVisibility(int accountVisibility) {this.accountVisibility = accountVisibility;}
+
+    public void setAccountStatus(int accountStatus) {this.accountStatus = accountStatus;}
+
+
+
+}
---
+++ Backend/demo/src/main/java/User/UserRepository.java
@@ -0,0 +1,11 @@
+package User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findById(int id);
+
+    @Transactional
+    void deleteById(int id);
+}
