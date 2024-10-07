21822fbc3405d4e3838904e7721cab94c2742e02
Griffin <148525371+griffinurban03@users.noreply.github.com>
2024-10-07 16:26:40 -0500

    added warning for missing login inputs, and deletion of text when text fields are typed in

--- Frontend/OpeningActivity/app/src/main/java/com/example/openingactivity/LoginActivity.java
+++ Frontend/OpeningActivity/app/src/main/java/com/example/openingactivity/LoginActivity.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -25,8 +27,11 @@ import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity {
 
+    final String GET_URL = "http://10.90.74.200:8080";
+
     Button buttonBack, buttonLogin, buttonForgotPassword;
     EditText inputUsername, inputPassword;
+    TextView textView;
 
 
     @Override
@@ -40,6 +45,7 @@ public class LoginActivity extends AppCompatActivity {
         buttonForgotPassword = findViewById(R.id.button_forgot_password);
         inputUsername = findViewById(R.id.login_input_username);
         inputPassword = findViewById(R.id.login_input_password);
+        textView = findViewById(R.id.textView2);
 
         // Button to take us back
         buttonBack.setOnClickListener(new View.OnClickListener() {
@@ -57,8 +63,14 @@ public class LoginActivity extends AppCompatActivity {
 
                 // Handle the login button click
 
-
+                if (!username.isEmpty() && !password.isEmpty()) {
+                    // Send GET request
+                    sendGetRequest(GET_URL + "/user/" + username);
+                } else {
+                    textView.setText("please fill in all fields*");
+                }
             }
+
         });
 
         buttonForgotPassword.setOnClickListener(new View.OnClickListener() {
@@ -68,7 +80,29 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        }
+        //method that sets TextView to nothing when username field is typed in
+        inputUsername.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                textView.setText("");
+                return false;
+            }
+        });
+
+        //method that sets TextView to nothing when password field is typed in
+        inputPassword.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                textView.setText("");
+                return false;
+            }
+        });
+
+
+    }
+
+    private void sendGetRequest(String s) {
+    }
 
 
 }
