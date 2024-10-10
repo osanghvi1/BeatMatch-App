7dcb5ce7e8fab6baae3eace5bdef4ac9bf718d64
Griffin <148525371+griffinurban03@users.noreply.github.com>
2024-10-09 20:34:48 -0500

    creating more in profile page, adding barebones delete, trying to fix Kassons code :(

--- Frontend/OpeningActivity/app/src/main/java/com/example/openingactivity/ProfileActivity.java
+++ Frontend/OpeningActivity/app/src/main/java/com/example/openingactivity/ProfileActivity.java
@@ -1,14 +1,21 @@
 package com.example.openingactivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ProfileActivity extends AppCompatActivity {
 
-    TextView textGetUser;
+    TextView textGetUser, textGetEmail;
+    Button deleteButton;
+    ExecutorService executorService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +23,22 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
         // Set up edge-to-edge display.
 
-        textGetUser = findViewById(R.id.text_get_user);
+        textGetUser = findViewById(R.id.text_get_user_ID);
+        textGetEmail = findViewById(R.id.text_get_user_email);
+        deleteButton = findViewById(R.id.button_delete_account);
+
+        executorService = Executors.newSingleThreadExecutor();
+
+
         textGetUser.setText("" + user.getUserID());
+        textGetEmail.setText("" + user.getUserEmail());
+
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO delete account
+            }
+        });
     }
 }
