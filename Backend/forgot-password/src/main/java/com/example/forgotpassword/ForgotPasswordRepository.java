12198683df25ab5d4e5f87a12410ce98976a74f4
osanghvi <ombakugan@gmail.com>
2024-10-09 19:04:00 -0500

    Delete folder

--- Backend/forgot-password/src/main/java/com/example/forgotpassword/ForgotPasswordRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.forgotpassword;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ForgotPasswordRepository extends JpaRepository<ForgotPassword, Integer> {
-    ForgotPassword findByUsername(String username);
-}
