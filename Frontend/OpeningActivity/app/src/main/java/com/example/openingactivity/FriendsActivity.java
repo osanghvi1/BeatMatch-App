87044399b6e7ad677d2d11ad971a8bc4687ef143
Griffin <148525371+griffinurban03@users.noreply.github.com>
2024-10-24 16:20:43 -0500

    building friends page, testing out listView for friends container

--- Frontend/OpeningActivity/app/src/main/java/com/example/openingactivity/FriendsActivity.java
+++ Frontend/OpeningActivity/app/src/main/java/com/example/openingactivity/FriendsActivity.java
@@ -2,7 +2,11 @@ package com.example.openingactivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,12 +16,16 @@ import java.util.concurrent.Executors;
 public class FriendsActivity extends AppCompatActivity implements Request {
     // UI elements
     Button buttonBack;
+    ListView lvFriends;
     ExecutorService executorService;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
         buttonBack = findViewById(R.id.button_friends_back);
+        lvFriends = findViewById(R.id.friends_list_view);
+
+        // Initialize the ExecutorService
         executorService = Executors.newSingleThreadExecutor();
 
         buttonBack.setOnClickListener(new View.OnClickListener() {
@@ -26,5 +34,20 @@ public class FriendsActivity extends AppCompatActivity implements Request {
                 finish();
             }
         });
+
+        // Fetch friends list from the server
+
+        String[] friendsList = {"Friend 1", "Friend 2", "Friend 3"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, friendsList);
+        lvFriends.setAdapter(adapter);
+        lvFriends.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(FriendsActivity.this, "Friend clicked: " + friendsList[position], Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
