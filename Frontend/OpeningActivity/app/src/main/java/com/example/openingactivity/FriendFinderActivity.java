8aad66f3e83239dbd9e86e1a2770699adb196ea8
Griffin <148525371+griffinurban03@users.noreply.github.com>
2024-10-25 15:41:03 -0500

    rewrote the methods for FriendFinder activity using ArrayList

--- Frontend/OpeningActivity/app/src/main/java/com/example/openingactivity/FriendFinderActivity.java
+++ Frontend/OpeningActivity/app/src/main/java/com/example/openingactivity/FriendFinderActivity.java
@@ -7,8 +7,11 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
-
+import java.util.ArrayList;
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -19,14 +22,15 @@ public class FriendFinderActivity extends AppCompatActivity implements Request {
     ListView lvEveryone;
     ExecutorService executorService;
 
+    ArrayList<String> friendsList = new ArrayList<>();
+    ArrayAdapter<String> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend_finder);
         buttonBack = findViewById(R.id.button_friends_back);
-        lvEveryone = findViewById(R.id.friends_list_view);
-
         executorService = Executors.newSingleThreadExecutor();
 
         buttonBack.setOnClickListener(new View.OnClickListener() {
@@ -36,26 +40,47 @@ public class FriendFinderActivity extends AppCompatActivity implements Request {
             }
         });
 
+        // TODO GET method for friends
 
-        String[] friendsList = {"Person 1", "Person 2", "Person 3", "Person 4", "Person 5", "Person 6", "Person 7", "Person 8", "Person 9", "Person 10"};
+        //dummy data for now
+        friendsList.add("Person 1");
+        friendsList.add("Person 2");
+        friendsList.add("Person 3");
+        friendsList.add("Person 4");
+        friendsList.add("Person 5");
 
-        // TODO would love a table where user ids, friend ids, and usernames are stored for easy access,
-        // could do a get method to get all friends, would be useful for chatting too.
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, friendsList);
+        lvEveryone = findViewById(R.id.friends_list_view);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, friendsList);
         lvEveryone.setAdapter(adapter);
+
         lvEveryone.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // Handle friend click
+                String selectedFriend = friendsList.get(position);
+                friendsList.remove(selectedFriend);
+                adapter.notifyDataSetChanged();
+                Toast.makeText(FriendFinderActivity.this, "Friend removed: " + selectedFriend, Toast.LENGTH_SHORT).show();
+
+                // POST request to add selectedFriend to friendsList
+                JSONObject json = new JSONObject();
+
+                try {
+                    json.put("friendID", selectedFriend);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
                 executorService.execute(new Runnable() {
                     @Override
                     public void run() {
-                        String response = sendRequest("POST", "/friends/" + user.getUserID() + "/" + friendsList[position], null);
+                        String response = sendRequest("POST", "/friends/" + user.getUserID(), selectedFriend);
+                        // put response in log for debugging
                     }
                 });
-                adapter.notifyDataSetChanged();
             }
         });
+
+
     }
 }
