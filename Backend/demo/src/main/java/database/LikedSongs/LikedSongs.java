ab998efda3b1c698b264613df18b2f2f0f11df28
osanghvi <ombakugan@gmail.com>
2024-10-30 21:11:16 -0400

    Added LikedSongs

---
+++ Backend/demo/src/main/java/database/LikedSongs/LikedSongs.java
@@ -0,0 +1,41 @@
+package database.LikedSongs;
+
+import jakarta.persistence.*;
+
+@Entity
+public class LikedSongs {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userID;
+
+    @Column(name = "song_id", nullable = false)
+    private Long songID;
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Long userID) {
+        this.userID = userID;
+    }
+
+    public Long getSongID() {
+        return songID;
+    }
+
+    public void setSongID(Long songID) {
+        this.songID = songID;
+    }
+}
