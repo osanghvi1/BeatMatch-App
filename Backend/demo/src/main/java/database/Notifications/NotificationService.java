f7ca00b7847bd1bff37b0c62c585b4eae4e8f4c8
Lawson <lport@iastate.edu>
2024-11-04 14:55:25 -0600

    adding support for notifications, testing is next

---
+++ Backend/demo/src/main/java/database/Notifications/NotificationService.java
@@ -0,0 +1,15 @@
+package database.Notifications;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationService {
+    @Autowired
+    private SimpMessagingTemplate template;
+    public void sendNotification(String message) {
+        template.convertAndSend("/topic/notifications", message);
+    }
+}
+
