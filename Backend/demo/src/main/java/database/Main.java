1923024b75c999ec8947f4a9e30e382e9a7b1f7d
Lawson <lport@iastate.edu>
2024-11-04 18:27:51 -0600

    testing this websocket is taking the life out of me

--- Backend/demo/src/main/java/database/Main.java
+++ Backend/demo/src/main/java/database/Main.java
@@ -20,29 +20,4 @@ public class Main {
         SpringApplication.run(Main.class, args);
 
     }
-
-    // Uncomment this section if you need to add dummy users and laptops
-    /*
-    @Bean
-    CommandLineRunner initUser(UserRepository userRepository, LaptopRepository laptopRepository) {
-        return args -> {
-            User user1 = new User("John", "john@somemail.com");
-            User user2 = new User("Jane", "jane@somemail.com");
-            User user3 = new User("Justin", "justin@somemail.com");
-            User user4 = new User("Lawson", "lport@iastate.edu");
-
-            Laptop laptop1 = new Laptop(2.5, 4, 8, "Lenovo", 300);
-            Laptop laptop2 = new Laptop(4.1, 8, 16, "Hp", 800);
-            Laptop laptop3 = new Laptop(3.5, 32, 32, "Dell", 2300);
-
-            user1.setLaptop(laptop1);
-            user2.setLaptop(laptop2);
-            user4.setLaptop(laptop3);
-
-            userRepository.save(user1);
-            userRepository.save(user2);
-            userRepository.save(user4);
-        };
-    }
-    */
 }
