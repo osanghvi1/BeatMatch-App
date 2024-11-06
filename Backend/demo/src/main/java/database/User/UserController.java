53025f937821bc4115e335ac335fe5a1f03d6388
Lawson <lport@iastate.edu>
2024-11-06 14:13:47 -0600

    literally added one comment

--- Backend/demo/src/main/java/database/User/UserController.java
+++ Backend/demo/src/main/java/database/User/UserController.java
@@ -15,7 +15,7 @@ import java.util.List;
     public class UserController {
 
     @Autowired
-        private UserRepository userRepository;
+    private UserRepository userRepository;
 
     private String success = "{\"message\":\"success\"}";
     private String failure = "{\"message\":\"failure\"}";
