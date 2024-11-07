57e90a9643c67e54e4edb17d036f9229a5a681e2
Lawson <lport@iastate.edu>
2024-11-07 16:43:21 -0600

    fixing formatting for merge

--- Backend/demo/src/main/java/database/User/UserController.java
+++ Backend/demo/src/main/java/database/User/UserController.java
@@ -1,6 +1,6 @@
 package database.User;
 
-import database.Notifications.NotificationService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import database.Notifications.NotificationService;
 
 import java.util.List;
 
@@ -17,6 +18,12 @@ import java.util.List;
 
     @Autowired
     private UserRepository userRepository;
+
+
+
+
+
+
 
     @Autowired
     private NotificationService notificationService;
