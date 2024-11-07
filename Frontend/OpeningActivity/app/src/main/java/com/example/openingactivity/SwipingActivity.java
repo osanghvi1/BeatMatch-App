d7ced139d99c2651976210d030a835351d374532
Griffin <148525371+griffinurban03@users.noreply.github.com>
2024-11-06 18:18:00 -0600

    Adding fling adapter view for containing methods for card entrance, exit, and swipe actions fro  screen

--- Frontend/OpeningActivity/app/src/main/java/com/example/openingactivity/SwipingActivity.java
+++ Frontend/OpeningActivity/app/src/main/java/com/example/openingactivity/SwipingActivity.java
@@ -1,15 +1,24 @@
 package com.example.openingactivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.lorentzos.flingswipe.SwipeFlingAdapterView;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class SwipingActivity extends AppCompatActivity implements Request {
 
     ExecutorService executorService;
+
+    private ArrayAdapter<String> adapter;
+    List<String> data;
+    SwipeFlingAdapterView flingAdapterView;
 
     //onCreate
     @Override
@@ -18,6 +27,44 @@ public class SwipingActivity extends AppCompatActivity implements Request {
         setContentView(R.layout.activity_swiping);
         executorService = Executors.newSingleThreadExecutor();
 
+        flingAdapterView = findViewById(R.id.swipe);
 
+        data=new ArrayList<>();
+        data.add("Song 1");
+        data.add("Song 2");
+        data.add("Song 3");
+        data.add("Song 4");
+        data.add("Song 5");
+
+        adapter = new ArrayAdapter<>(SwipingActivity.this, R.layout.item, R.id.textSongData, data);
+
+        flingAdapterView.setAdapter(adapter);
+
+        flingAdapterView.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
+            @Override
+            public void removeFirstObjectInAdapter() {
+                
+            }
+
+            @Override
+            public void onLeftCardExit(Object o) {
+
+            }
+
+            @Override
+            public void onRightCardExit(Object o) {
+
+            }
+
+            @Override
+            public void onAdapterAboutToEmpty(int i) {
+
+            }
+
+            @Override
+            public void onScroll(float v) {
+
+            }
+        });
     }
 }
