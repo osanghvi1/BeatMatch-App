74f4d415f4bd83eac00147238cd0008fa22e8786
Griffin <148525371+griffinurban03@users.noreply.github.com>
2024-11-06 18:54:13 -0600

    swiping feature demo working, added 2 new methods for creating a song and queuing multiple songs

--- Frontend/OpeningActivity/app/src/main/java/com/example/openingactivity/SwipingActivity.java
+++ Frontend/OpeningActivity/app/src/main/java/com/example/openingactivity/SwipingActivity.java
@@ -1,7 +1,10 @@
 package com.example.openingactivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,6 +23,29 @@ public class SwipingActivity extends AppCompatActivity implements Request {
     List<String> data;
     SwipeFlingAdapterView flingAdapterView;
 
+    public String getNewSong(int k) {
+
+        //Make request to Deezer for a song
+
+        //Check with backend Liked songs Table AND Disliked songs table to see if we've seen it already
+
+        //if we have, try again
+
+        //if we have not, return the queue
+
+
+        String song = "Song " + k;
+        return song;
+    }
+
+    public void createSongQueue() {
+        int k=0;
+        while(k<=5){
+            data.add(getNewSong(k));
+            k++;
+        }
+    }
+
     //onCreate
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,11 +56,9 @@ public class SwipingActivity extends AppCompatActivity implements Request {
         flingAdapterView = findViewById(R.id.swipe);
 
         data=new ArrayList<>();
-        data.add("Song 1");
-        data.add("Song 2");
-        data.add("Song 3");
-        data.add("Song 4");
-        data.add("Song 5");
+
+        createSongQueue();
+
 
         adapter = new ArrayAdapter<>(SwipingActivity.this, R.layout.item, R.id.textSongData, data);
 
@@ -43,22 +67,23 @@ public class SwipingActivity extends AppCompatActivity implements Request {
         flingAdapterView.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
             @Override
             public void removeFirstObjectInAdapter() {
-                
+                data.remove(0);
+                adapter.notifyDataSetChanged();
             }
 
             @Override
             public void onLeftCardExit(Object o) {
-
+                Toast.makeText(SwipingActivity.this, "Dislike", Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onRightCardExit(Object o) {
-
+                Toast.makeText(SwipingActivity.this, "Like", Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onAdapterAboutToEmpty(int i) {
-
+                createSongQueue();
             }
 
             @Override
@@ -66,5 +91,42 @@ public class SwipingActivity extends AppCompatActivity implements Request {
 
             }
         });
+
+
+        flingAdapterView.setOnItemClickListener(new SwipeFlingAdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClicked(int i, Object o) {
+                Toast.makeText(SwipingActivity.this, "Item Clicked " + data.get(i), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button LikeButton, DislikeButton, RefreshButton;
+
+        LikeButton = findViewById(R.id.like);
+        DislikeButton = findViewById(R.id.dislike);
+        RefreshButton = findViewById(R.id.refresh);
+
+        LikeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flingAdapterView.getTopCardListener().selectRight();
+            }
+        });
+
+        DislikeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flingAdapterView.getTopCardListener().selectLeft();
+            }
+        });
+
+        RefreshButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createSongQueue();
+            }
+        });
+
+
     }
 }
