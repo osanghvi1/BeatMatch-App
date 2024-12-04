9eb249d2c32698fde1ee9f365ac29a97f28040c1
Griffin <148525371+griffinurban03@users.noreply.github.com>
2024-12-04 17:08:29 -0600

    working more on admin page, setting up return button and first admin controls

--- Frontend/OpeningActivity/app/src/main/java/com/example/openingactivity/AdminActivity.java
+++ Frontend/OpeningActivity/app/src/main/java/com/example/openingactivity/AdminActivity.java
@@ -1,14 +1,39 @@
 package com.example.openingactivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AdminActivity extends AppCompatActivity implements Request {
 
+
+    //XML elements
+    TextView textViewAdminHeader;
+    Button button_admin_return;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+        //map XML elements to UI elements
+        TextView textViewAdminHeader = findViewById(R.id.textViewAdminHeader);
+        Button button_admin_return = findViewById(R.id.button_admin_return);
+
+        /**
+         * This code is for the admin settings back button
+         * returns the user to the profile page
+         */
+        button_admin_return.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminActivity.this, ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
