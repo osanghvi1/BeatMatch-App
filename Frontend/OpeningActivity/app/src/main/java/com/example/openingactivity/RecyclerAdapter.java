af198883f3bca2349f4765761d1a87f6cfb58d45
Griffin <148525371+griffinurban03@users.noreply.github.com>
2024-12-10 15:59:58 -0600

    adding executor dependancies to recycler adapter

--- Frontend/OpeningActivity/app/src/main/java/com/example/openingactivity/RecyclerAdapter.java
+++ Frontend/OpeningActivity/app/src/main/java/com/example/openingactivity/RecyclerAdapter.java
@@ -10,12 +10,15 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyViewHolder> {
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyViewHolder> implements Request {
     private ArrayList<Event> eventList;
 
     public RecyclerAdapter(ArrayList<Event> eventList) {
@@ -23,6 +26,8 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
+        ExecutorService executorService;
+
         private TextView eventName;
         private TextView eventHost;
         private TextView eventLocation;
@@ -40,19 +45,20 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
             eventThumbnail = itemView.findViewById(R.id.imageView_event_thumbnail);
             joinButton = itemView.findViewById(R.id.button_event_join);
             cost = itemView.findViewById(R.id.textView_event_price);
-
+            
+            executorService = Executors.newSingleThreadExecutor();
         }
     }
 
     @NonNull
     @Override
-    public RecyclerAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_view_row, parent, false);
         return new MyViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerAdapter.MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         String eventName = eventList.get(position).getEventName();
         String eventHost = eventList.get(position).getEventHost();
         String eventLocation = eventList.get(position).getEventLocation();
@@ -67,11 +73,16 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
         holder.eventDate.setText(eventDate);
         holder.eventThumbnail.setImageDrawable(eventThumbnail);
         holder.cost.setText("$" + String.valueOf(cost));
+
+        holder.executorService = Executors.newSingleThreadExecutor();
+
         joinButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Make a toast saying which event was clicked
                 Toast.makeText(v.getContext(), "You've RSVP'd to " + eventName + "!", Toast.LENGTH_SHORT).show();
+
+                // TODO add event to RSVP table
             }
         });
     }
