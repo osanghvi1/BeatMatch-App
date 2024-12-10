ee105922da55d64248d499c5d680c59936a1ba82
Griffin <148525371+griffinurban03@users.noreply.github.com>
2024-12-09 18:07:42 -0600

    added join functionality to event cards, custom images

--- Frontend/OpeningActivity/app/src/main/java/com/example/openingactivity/Event.java
+++ Frontend/OpeningActivity/app/src/main/java/com/example/openingactivity/Event.java
@@ -1,16 +1,20 @@
 package com.example.openingactivity;
 
+import android.graphics.drawable.Drawable;
+
 public class Event {
+    private Drawable thumbnail;
     private String name;
     private String host;
     private String location;
     private String date;
 
-    public Event(String name, String host, String location, String date) {
+    public Event(String name, String host, String location, String date, Drawable thumbnail) {
         this.name = name;
         this.host = host;
         this.location = location;
         this.date = date;
+        this.thumbnail = thumbnail;
     }
 
     // Getters and setters for the event attributes
@@ -37,5 +41,11 @@ public class Event {
     }
     public void setEventDate(String date) {
         this.date = date;
+    }
+    public Drawable getEventThumbnail() {
+        return thumbnail;
+    }
+    public void setEventThumbnail(Drawable thumbnail) {
+        this.thumbnail = thumbnail;
     }
 }
--- Frontend/OpeningActivity/app/src/main/java/com/example/openingactivity/RecyclerAdapter.java
+++ Frontend/OpeningActivity/app/src/main/java/com/example/openingactivity/RecyclerAdapter.java
@@ -1,11 +1,16 @@
 package com.example.openingactivity;
 
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -19,10 +24,21 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         private TextView eventName;
+        private TextView eventHost;
+        private TextView eventLocation;
+        private TextView eventDate;
+        private ImageView eventThumbnail;
+        private Button joinButton;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             eventName = itemView.findViewById(R.id.textView_event_name);
+            eventHost = itemView.findViewById(R.id.textView_event_host);
+            eventLocation = itemView.findViewById(R.id.textView_event_location);
+            eventDate = itemView.findViewById(R.id.textView_event_date);
+            eventThumbnail = itemView.findViewById(R.id.imageView_event_thumbnail);
+            joinButton = itemView.findViewById(R.id.button_event_join);
+
         }
     }
 
@@ -36,8 +52,24 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
     @Override
     public void onBindViewHolder(@NonNull RecyclerAdapter.MyViewHolder holder, int position) {
         String eventName = eventList.get(position).getEventName();
+        String eventHost = eventList.get(position).getEventHost();
+        String eventLocation = eventList.get(position).getEventLocation();
+        String eventDate = eventList.get(position).getEventDate();
+        Drawable eventThumbnail = eventList.get(position).getEventThumbnail();
+        Button joinButton = holder.joinButton;
 
         holder.eventName.setText(eventName);
+        holder.eventHost.setText(eventHost);
+        holder.eventLocation.setText(eventLocation);
+        holder.eventDate.setText(eventDate);
+        holder.eventThumbnail.setImageDrawable(eventThumbnail);
+        joinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Make a toast saying which event was clicked
+                Toast.makeText(v.getContext(), "Joined " + eventName, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
